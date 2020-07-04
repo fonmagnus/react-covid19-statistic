3b4984a25b7a3070f301569ddf809b6f7005877a
fonmagnus <arnold777armageddon@gmail.com>
2020-07-04 08:24:09 +0700

    add icon

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./Logo.module.css";
-
+import { ReactComponent as Svg } from "./coronavirus.svg";
 const Logo = () => {
   return (
     <div>
-      <h1>COVID-19 Statistic</h1>
+      <h1>COVID-19 Statistic (Beta)</h1>
     </div>
   );
 };
